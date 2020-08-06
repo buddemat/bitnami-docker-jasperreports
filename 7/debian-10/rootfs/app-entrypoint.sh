1e26dac086ea03dbd50df5a5d80755dd2d7556de
buddemat <budde@teco.edu>
2020-08-06 16:49:53 +0200

    Corrected error in cp command so that subdirectories in the customcfg folder will now be copied as well.

--- 7/debian-10/rootfs/app-entrypoint.sh
+++ 7/debian-10/rootfs/app-entrypoint.sh
@@ -10,7 +10,7 @@ if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
     info "Starting gosu... "
     if [ -d "/etc/opt/bitnami/jasperreports/config/" ]; then
         if ! [ -z "$(ls -A /etc/opt/bitnami/jasperreports/config/)" ]; then 
-            cp "/etc/opt/bitnami/jasperreports/config/"* "/opt/bitnami/jasperreports/WEB-INF/"
+            cp -r "/etc/opt/bitnami/jasperreports/config/"* "/opt/bitnami/jasperreports/WEB-INF/"
         fi
     fi
 fi
